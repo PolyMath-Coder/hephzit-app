e3f62c89241689d8aabec55a4491f6b0adccf737
PolyMath-Coder <you@example.com>
2024-04-26 18:25:45 +0100

    feat: dockerize app

--- apps/grpc/src/main.ts
+++ apps/grpc/src/main.ts
@@ -3,6 +3,6 @@ import { GrpcModule } from './grpc.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GrpcModule);
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();
